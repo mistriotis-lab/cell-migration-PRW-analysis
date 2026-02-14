f0e85e73769e20c264990f4dc8cac17b2acc6780
ECoker <elc0055@auburn.edu>
2026-02-14 13:17:03 -0600

    Updated licenses to include 3rd party code attributions

--- src/multi_file_msd_process.m
+++ src/multi_file_msd_process.m
@@ -22,186 +22,186 @@ function [] = multi_file_msd_process()
     %   parsing
     %
 
-clear all
-
-tic
-
-% Minimum track length required for analysis
-min_length = 6;
-% Target R^2 value to assign a status of "Good" fit
-target_r2_val = 0.88;
-
-% Get path to folder of data to be analyzed
-folderPath = uigetdir(pwd, 'Select a folder to be analyzed:'); 
-
-% Lists all files in folderPath
-filesStruct = dir(folderPath);
-% Removes the current_dir and parent_dir objects from filesStruct
-filesStruct = filesStruct(~ismember({filesStruct.name}, {'.', '..'}));
-% Removes any subdirectories
-filesStruct = filesStruct(~[filesStruct.isdir]);
-
-% Loop over all remaining files
-for f = 1:length(filesStruct)
-
-    % Holds just the file's name 
-    baseFileName = filesStruct(f).name;
-
-    fprintf('Now processing %s\n', baseFileName);
-
-    % Holds the full path to the file
-    full_table_path = fullfile(filesStruct(f).folder, baseFileName);    
-
-    % If an output file from previous analysis exists in directory, skip
-    if startsWith(baseFileName, 'PROCESSED')
-        continue
-    end
-
-    % Load data from file
-    data = readtable(full_table_path);
-
-    % Check if time column label matches anticipated format
-    % If not, the label is changed within the data structure
-    if ismember('t_mins_', data.Properties.VariableNames)
-        data.Properties.VariableNames{'t_mins_'} = 't_min_';
-    end
-
-    %% Pathing
-    % path to an output directory within the data directory
-    outDir = fullfile(folderPath, 'temp_out');
-    % Print the output directory path
-    disp(outDir)
-    % Check if outDir exists, create folder if it does not
-    if ~exist(outDir, 'dir')
-        mkdir(outDir)
-    end
-
-    % Use fileparts to strip the extension of the data file
-    [~, nameOnly, ~] = fileparts(baseFileName);
+    clear all
     
-    % Force the output calculation file to be .xlsx
-    cleanName = regexprep(nameOnly, '\.', '_'); 
-    cleanName = strtrim(cleanName);
-    outfile_name = ['PROCESSED_', cleanName, '.xlsx'];
-
-    outFile = fullfile(outDir, outfile_name);
-
-    %% File Statistics
-    % Get all IDs of cells contained in the file
-    uniqueIDs = unique(data.TID);
-    % Make an iterable based on length of uniqueIDs
-    numTracks = length(uniqueIDs);
+    tic
     
-    % Determine maximum track length for building the MSD table
-    trackCounts = groupsummary(data, 'TID');
-    maxLags = max(trackCounts.GroupCount) - 1;
+    % Minimum track length required for analysis
+    min_length = 6;
+    % Target R^2 value to assign a status of "Good" fit
+    target_r2_val = 0.88;
     
-    % WARNING: ASSUMES CONSISTENT TIME STEP INTERVALS
-    timeStep = data.t_min_(2) - data.t_min_(1);
+    % Get path to folder of data to be analyzed
+    folderPath = uigetdir(pwd, 'Select a folder to be analyzed:'); 
     
-    %% Output Setup
-    %  Set up an empty table for storing speed, velocity, persistence
-    stats = table( ...
-        nan(numTracks,1), nan(numTracks,1), nan(numTracks,1), ...
-        nan(numTracks,1), nan(numTracks,1), nan(numTracks,1), ...
-        nan(numTracks,1), nan(numTracks,1), nan(numTracks,1), ...
-        nan(numTracks,1), nan(numTracks,1), strings(numTracks,1), ...
-        'VariableNames', { ...
-        'Cell_ID', 'O_Speed', 'O_Velocity', ...
-        'O_Persistence', 'O_Displacement', 'N_Speed', ...
-        'N_Persistence', 'N_SE', 'N_GoF', ...
-        'Fitted_Gamma', 'Fitted_Alpha','PRW_Fit_Flag' ...
-        });
+    % Lists all files in folderPath
+    filesStruct = dir(folderPath);
+    % Removes the current_dir and parent_dir objects from filesStruct
+    filesStruct = filesStruct(~ismember({filesStruct.name}, {'.', '..'}));
+    % Removes any subdirectories
+    filesStruct = filesStruct(~[filesStruct.isdir]);
     
-    % Set up an empty table for storing MSD of each cell
-    msdVarNames = [{'Tau_min'}, arrayfun(@(x) sprintf('TID_%d', x), uniqueIDs', 'UniformOutput', false)];
-    msd_table = array2table(nan(maxLags, numTracks + 1), 'VariableNames', msdVarNames);
-    % Fill in all possible tau values
-    msd_table.Tau_min = (1:maxLags)' * timeStep;
+    % Loop over all remaining files
+    for f = 1:length(filesStruct)
     
-    %% Main Loop
-    for c = 1:numTracks
+        % Holds just the file's name 
+        baseFileName = filesStruct(f).name;
     
-        disp(['Processing cell: ', num2str(c)])
+        fprintf('Now processing %s\n', baseFileName);
     
-        % Downselect to the current cell
-        currentTrack = data(data.TID == uniqueIDs(c), :);
+        % Holds the full path to the file
+        full_table_path = fullfile(filesStruct(f).folder, baseFileName);    
     
-        % If length of track is less than min_length, then skip analysis
-        if height(currentTrack) <= min_length
-            % All values were initialized as NaN, so we just continue
-            stats.Cell_ID(c)        = c;
-            stats.PRW_Fit_Flag(c)       = "No_Fit";
+        % If an output file from previous analysis exists in directory, skip
+        if startsWith(baseFileName, 'PROCESSED')
             continue
         end
-        
-        % Get the total distance traveled (micron)
-        totalDistance = currentTrack.Len_micron_(end);
     
-        % Calculate the elapsed time of cell track (all in minutes)
-        timeStart       = currentTrack.t_min_(1);
-        timeEnd         = currentTrack.t_min_(end);
-        delta_t         = currentTrack.t_min_(3) - currentTrack.t_min_(2) ;
-        elapsedTime     = timeEnd - timeStart;
+        % Load data from file
+        data = readtable(full_table_path);
     
-        % Assign start and end points (all in microns)
-        startX      = currentTrack.x_micron_(1);
-        startY      = currentTrack.y_micron_(1);
-        endX        = currentTrack.x_micron_(end);
-        endY        = currentTrack.y_micron_(end);
-    
-        % Calculate final total displacement (in microns)
-        displacement = sqrt((endX - startX)^2 + (endY - startY)^2);
-        
-        % Calculate MSD and populate table (microns^2)
-        cellMSD = calculateMSD(currentTrack.x_micron_, currentTrack.y_micron_);
-        msd_table{1:length(cellMSD), c+1}  = cellMSD;
-    
-        % Model fitting
-        taus = (1:length(cellMSD))' * timeStep;
-   
-        % Fit PRW using MSD data
-        [fit_persistence,fit_speed,SE,gof] = msd2pse0(cellMSD, delta_t, 2);
-        r_squared = gof.rsquare;
-
-        % Fit alpha and Gamma parameters for power law
-        [alpha, gamma] = fitAlpha(taus, cellMSD);
-    
-        % Append values to statistical table
-        stats.Cell_ID(c)          = c;  % Integer
-    
-        % Legacy Calculation Methods
-        stats.O_Speed(c)          = totalDistance / elapsedTime * 60; % um/hr
-        stats.O_Velocity(c)       = displacement / elapsedTime * 60; % um/hr
-        % This persistence produces a dimensionless quantity, some ratio
-        stats.O_Persistence(c)    = stats.O_Velocity(c) / stats.O_Speed(c); % -
-        stats.O_Displacement(c)   = displacement;   % um
-    
-        % Model Fit Parameters
-        stats.Fitted_Gamma(c)     = gamma;
-        stats.Fitted_Alpha(c)     = alpha;
-        stats.N_Speed(c)          = fit_speed * 60;   % um / hr
-        % Whereas this persistence is a calculated quantity with units. Here,
-        % it is converted and report in hours
-        stats.N_Persistence(c)    = fit_persistence / 60;   % hr
-        stats.N_SE(c)             = SE;
-        stats.N_GoF(c)            = r_squared;      % R^2
-        if r_squared >= target_r2_val
-            stats.PRW_Fit_Flag(c)     = "Pass";
-        else
-            stats.PRW_Fit_Flag(c)     = "Fail";
+        % Check if time column label matches anticipated format
+        % If not, the label is changed within the data structure
+        if ismember('t_mins_', data.Properties.VariableNames)
+            data.Properties.VariableNames{'t_mins_'} = 't_min_';
         end
+    
+        %% Pathing
+        % path to an output directory within the data directory
+        outDir = fullfile(folderPath, 'temp_out');
+        % Print the output directory path
+        disp(outDir)
+        % Check if outDir exists, create folder if it does not
+        if ~exist(outDir, 'dir')
+            mkdir(outDir)
+        end
+    
+        % Use fileparts to strip the extension of the data file
+        [~, nameOnly, ~] = fileparts(baseFileName);
+        
+        % Force the output calculation file to be .xlsx
+        cleanName = regexprep(nameOnly, '\.', '_'); 
+        cleanName = strtrim(cleanName);
+        outfile_name = ['PROCESSED_', cleanName, '.xlsx'];
+    
+        outFile = fullfile(outDir, outfile_name);
+    
+        %% File Statistics
+        % Get all IDs of cells contained in the file
+        uniqueIDs = unique(data.TID);
+        % Make an iterable based on length of uniqueIDs
+        numTracks = length(uniqueIDs);
+        
+        % Determine maximum track length for building the MSD table
+        trackCounts = groupsummary(data, 'TID');
+        maxLags = max(trackCounts.GroupCount) - 1;
+        
+        % WARNING: ASSUMES CONSISTENT TIME STEP INTERVALS
+        timeStep = data.t_min_(2) - data.t_min_(1);
+        
+        %% Output Setup
+        %  Set up an empty table for storing speed, velocity, persistence
+        stats = table( ...
+            nan(numTracks,1), nan(numTracks,1), nan(numTracks,1), ...
+            nan(numTracks,1), nan(numTracks,1), nan(numTracks,1), ...
+            nan(numTracks,1), nan(numTracks,1), nan(numTracks,1), ...
+            nan(numTracks,1), nan(numTracks,1), strings(numTracks,1), ...
+            'VariableNames', { ...
+            'Cell_ID', 'O_Speed', 'O_Velocity', ...
+            'O_Persistence', 'O_Displacement', 'N_Speed', ...
+            'N_Persistence', 'N_SE', 'N_GoF', ...
+            'Fitted_Gamma', 'Fitted_Alpha','PRW_Fit_Flag' ...
+            });
+        
+        % Set up an empty table for storing MSD of each cell
+        msdVarNames = [{'Tau_min'}, arrayfun(@(x) sprintf('TID_%d', x), uniqueIDs', 'UniformOutput', false)];
+        msd_table = array2table(nan(maxLags, numTracks + 1), 'VariableNames', msdVarNames);
+        % Fill in all possible tau values
+        msd_table.Tau_min = (1:maxLags)' * timeStep;
+        
+        %% Main Loop
+        for c = 1:numTracks
+        
+            disp(['Processing cell: ', num2str(c)])
+        
+            % Downselect to the current cell
+            currentTrack = data(data.TID == uniqueIDs(c), :);
+        
+            % If length of track is less than min_length, then skip analysis
+            if height(currentTrack) <= min_length
+                % All values were initialized as NaN, so we just continue
+                stats.Cell_ID(c)        = c;
+                stats.PRW_Fit_Flag(c)       = "No_Fit";
+                continue
+            end
+            
+            % Get the total distance traveled (micron)
+            totalDistance = currentTrack.Len_micron_(end);
+        
+            % Calculate the elapsed time of cell track (all in minutes)
+            timeStart       = currentTrack.t_min_(1);
+            timeEnd         = currentTrack.t_min_(end);
+            delta_t         = currentTrack.t_min_(3) - currentTrack.t_min_(2) ;
+            elapsedTime     = timeEnd - timeStart;
+        
+            % Assign start and end points (all in microns)
+            startX      = currentTrack.x_micron_(1);
+            startY      = currentTrack.y_micron_(1);
+            endX        = currentTrack.x_micron_(end);
+            endY        = currentTrack.y_micron_(end);
+        
+            % Calculate final total displacement (in microns)
+            displacement = sqrt((endX - startX)^2 + (endY - startY)^2);
+            
+            % Calculate MSD and populate table (microns^2)
+            cellMSD = calculateMSD(currentTrack.x_micron_, currentTrack.y_micron_);
+            msd_table{1:length(cellMSD), c+1}  = cellMSD;
+        
+            % Model fitting
+            taus = (1:length(cellMSD))' * timeStep;
+       
+            % Fit PRW using MSD data
+            [fit_persistence,fit_speed,SE,gof] = msd2pse0(cellMSD, delta_t, 2);
+            r_squared = gof.rsquare;
+    
+            % Fit alpha and Gamma parameters for power law
+            [alpha, gamma] = fitAlpha(taus, cellMSD);
+        
+            % Append values to statistical table
+            stats.Cell_ID(c)          = c;  % Integer
+        
+            % Legacy Calculation Methods
+            stats.O_Speed(c)          = totalDistance / elapsedTime * 60; % um/hr
+            stats.O_Velocity(c)       = displacement / elapsedTime * 60; % um/hr
+            % This persistence produces a dimensionless quantity, some ratio
+            stats.O_Persistence(c)    = stats.O_Velocity(c) / stats.O_Speed(c); % -
+            stats.O_Displacement(c)   = displacement;   % um
+        
+            % Model Fit Parameters
+            stats.Fitted_Gamma(c)     = gamma;
+            stats.Fitted_Alpha(c)     = alpha;
+            stats.N_Speed(c)          = fit_speed * 60;   % um / hr
+            % Whereas this persistence is a calculated quantity with units. Here,
+            % it is converted and report in hours
+            stats.N_Persistence(c)    = fit_persistence / 60;   % hr
+            stats.N_SE(c)             = SE;
+            stats.N_GoF(c)            = r_squared;      % R^2
+            if r_squared >= target_r2_val
+                stats.PRW_Fit_Flag(c)     = "Pass";
+            else
+                stats.PRW_Fit_Flag(c)     = "Fail";
+            end
+        
+        end
+        
+        writetable(stats, outFile,  'Sheet', 'ParamSummary')
+        writetable(msd_table, outFile, 'Sheet', 'MSD_Data')
+        disp(stats)
     
     end
     
-    writetable(stats, outFile,  'Sheet', 'ParamSummary')
-    writetable(msd_table, outFile, 'Sheet', 'MSD_Data')
-    disp(stats)
-
-end
-
-toc
-
+    toc
+    
 end
 
 %% -- LOCAL FUNCTIONS --
@@ -263,6 +263,34 @@ function msd = calculateMSD(x, y)
 end
 
 function [P,S,SE,gof]=msd2pse0(msd,dt,dim)
+% This function is adapted from P.-H. Wu, A. Giri, and D. Wirtz, 
+% "Statistical analysis of cell migration in 3D using the anisotropic 
+% persistent random walk model," *Nat Protoc*, vol. 10, no. 3, pp. 517–527,
+% Mar. 2015, doi: [10.1038/nprot.2015.030](10.1038/nprot.2015.030)" and is
+% used under the MIT License.
+%
+% MIT License
+% 
+% Copyright (c) 2026 DeepBioVision
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+
 %******************************************************
 % Characterizing the msd by persistent Random walk model.
 % output: persistence, speed, and positioning error, goodness of fit.
--- src/single_file_msd_process.m
+++ src/single_file_msd_process.m
@@ -235,6 +235,34 @@ function msd = calculateMSD(x, y)
 end
 
 function [P,S,SE,gof]=msd2pse0(msd,dt,dim)
+% This function is adapted from P.-H. Wu, A. Giri, and D. Wirtz, 
+% "Statistical analysis of cell migration in 3D using the anisotropic 
+% persistent random walk model," *Nat Protoc*, vol. 10, no. 3, pp. 517–527,
+% Mar. 2015, doi: [10.1038/nprot.2015.030](10.1038/nprot.2015.030)" and is
+% used under the MIT License.
+%
+% MIT License
+% 
+% Copyright (c) 2026 DeepBioVision
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+
 %******************************************************
 % Characterizing the msd by persistent Random walk model.
 % output: persistence, speed, and positioning error, goodness of fit.
